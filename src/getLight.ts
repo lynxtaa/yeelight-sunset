7d7572d71866a46a732a5b510422569197863e40
Alex Kondratyuk <lynxtaa@gmail.com>
2021-11-13 13:47:00 +0300

    chore: warn on WSL

--- src/getLight.ts
+++ src/getLight.ts
@@ -1,4 +1,10 @@
+/* eslint-disable no-console */
+import isWsl from 'is-wsl'
 import Yeelight from 'yeelight2'
+
+if (isWsl) {
+	console.warn('Warning: Running in WSL may cause issues with lightbulb discovery')
+}
 
 export function getLight(): Promise<Yeelight.Light> {
 	return new Promise(resolve => {
